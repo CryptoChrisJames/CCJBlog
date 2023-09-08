128a2b01503f24d95ca0a0c3c7f3c61b11ed6e67
Christopher Smith <22749214+CryptoChrisJames@users.noreply.github.com>
2023-09-07 19:40:58 -0500

    Save state.

--- ccj-blog/src/admin.rs
+++ ccj-blog/src/admin.rs
@@ -1,46 +1,46 @@
-
+use std::f32::consts::E;
 use rocket::{Error, http::Status};
-use rocket_dyn_templates::Template;
-use rocket::response::Result;
+use rocket_dyn_templates::{Template, context};
 use bcrypt::{DEFAULT_COST, hash, verify};
 use rocket::form::Form;
-use crate::domain::User;
-use crate::repo::UserRepo;
+use rusqlite::Connection;
 
 #[derive(FromForm)]
-struct Login {
+struct LoginInfo {
     username: String,
     password: String,
 }
 
+enum UserResult {
+    User(String, String),
+    NotFound(String),
+}
+
+#[catch(401)]
+pub fn unauthorized() -> String {
+    "Unauthorized".into()
+}
+
+#[catch(404)]
+pub fn not_found() -> String {
+    "User Not Found".into()
+}
+
+#[catch(500)]
+pub fn server_issue() -> String {
+    "Server Issue".into()
+}
+
 #[post("/admin/login", data = "<login>")]
-pub fn login(login: Form<Login>) -> Template {
+pub fn login(login: Form<LoginInfo>) -> Result<Template, Status> {
     let loginInfo = login.into_inner();
-    let pwHash = hash(loginInfo.password, DEFAULT_COST).unwrap();
-    let user = User::new(&loginInfo.username, &pwHash);
-    let userRepo = UserRepo { user: user };
-    let foundUser: Result<User, Error> = userRepo.get();
-    match foundUser {
-        Ok(user) => {
-            let result = verify(&user.password, &pwHash);
-            match result {
-                Ok(valid) => {
-                    if valid {
-                        let context = user;
-                        return Ok(Template::render("admin/dashboard", &context));
-                    }
-                    return Err(Status::Unauthorized);
-                },
-                Err(e) => {
-                    println!("{:?}", e);
-                    return Err(Status::InternalServerError);
-                },
-            }
-        },
-        Err(e) => {
-            println!("{:?}", e);
-            return Status::InternalServerError;
-        },
+    if authenticate(&loginInfo.username, &loginInfo.password) {
+        let context = context! {
+            username: &loginInfo.username,
+        };
+        return Ok(Template::render("admin/dashboard", &context));
+    } else {
+        Err(rocket::http::Status::Unauthorized)
     }
 }
 
@@ -60,4 +60,53 @@ pub fn admin() -> Template {
 //         return Status::Created;
 //     }
 //     Status::InternalServerError
+// }
+
+fn getDb() -> Connection {
+    return Connection::open("./db/ccj-blog.db").unwrap();
+}
+
+fn authenticate(authUsername:&str, authPassword: &str) -> bool {
+    let existing_user: UserResult = get_user(authUsername);
+    match existing_user {
+        UserResult::User(username, password) => {
+            let hashed = hash(authPassword, DEFAULT_COST).unwrap();
+            match verify(hashed, &password) {
+                Ok(valid) => valid,
+                Err(e) => {
+                    println!("{:?}", e);
+                    return false;
+                },
+            }
+        },
+        UserResult::NotFound(e) => {
+            println!("{:?}", e);
+            return false;
+        },
+    }
+}
+
+fn get_user(username:&str) -> UserResult {
+    let db = getDb();
+    let stmt = &mut db.prepare("SELECT * FROM admin WHERE username=?").unwrap();
+    let existing_user = match stmt.query_row(&[&username], |row| {
+        Ok(((row.get(0)?, row.get(1)?)))
+    }) {
+        Ok(user) => {
+            return UserResult::User(user.0, user.1);
+        },
+        Err(rusqlite::Error::QueryReturnedNoRows) => UserResult::NotFound("User not found".into()),
+        Err(_) => UserResult::NotFound("User not found".into()),  // Treat other errors as "not found"
+    };
+    existing_user
+}
+
+// pub fn create(&self) -> bool {
+//     let db = getDb();
+//     let mut stmt = db.prepare("INSERT INTO admin (username, password) VALUES (?, ?)").unwrap();
+//     let result = stmt.execute(&[&self.user.username, &self.user.password]);
+//     return match result {
+//         Ok(_) => true,
+//         Err(_) => false,
+//     }
 // }
--- ccj-blog/src/main.rs
+++ ccj-blog/src/main.rs
@@ -7,13 +7,12 @@ use rocket_dyn_templates::Template;
 
 mod home;
 mod admin;
-mod domain;
-mod repo;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/static", FileServer::from("static"))
         .mount("/", routes![home::index, admin::admin, admin::login])
+        .register("/admin", catchers![admin::not_found, admin::server_issue, admin::unauthorized])
         .attach(Template::fairing())
 }
