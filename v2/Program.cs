84ff50b85b65c763a96c907db185a7d65a487f57
Christopher Smith <22749214+CryptoChrisJames@users.noreply.github.com>
2024-03-18 11:22:07 -0500

    setting up html pages.

--- v2/Program.cs
+++ v2/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Chris!");
+app.UseStaticFiles();
+
+app.MapGet("/", () => {
+    var filePath = Path.Combine(app.Environment.WebRootPath, "pages/index.html");
+    return Results.File(filePath, "text/html");
+});
+
+app.MapGet("/admin", () => {
+    var filePath = Path.Combine(app.Environment.WebRootPath, "pages/admin/index.html");
+    return Results.File(filePath, "text/html");
+});
+
 
 app.Run();
--- v2/obj/Debug/net8.0/v2.AssemblyInfo.cs
+++ v2/obj/Debug/net8.0/v2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9af685e76539b33bbe914a2bbd593a3a9937794e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b8e21abbb2cff3c006ed7983c54fc1215fa169")]
 [assembly: System.Reflection.AssemblyProductAttribute("v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
